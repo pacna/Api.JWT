99693eb22b93f2aed668347e1f721873bc98d513
pacna <paulc1231@gmail.com>
2020-09-03 00:30:18 -0400

    Add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller()
+@ApiTags("jwt")
+@Controller("jwt")
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createJwt(): void {
+    console.log('foo');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  
+  const app: INestApplication = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+
+  const options = new DocumentBuilder()
+    .setTitle('Api.Jwt')
+    .setDescription('A simple jwt service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(5000);
+  
 }
+
 bootstrap();
