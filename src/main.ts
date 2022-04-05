364a7bc9cd8ca42254428f9b82b21407623951a6
pacna <31336845+pacna@users.noreply.github.com>
2022-04-05 14:34:51 -0400

    Fix some security vulnerabilities

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,21 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  
   const app: INestApplication = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
     .setTitle('Api.Jwt')
     .setDescription('A simple jwt service')
     .setVersion('1.0')
+    .setBasePath('/')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
 
-  SwaggerModule.setup('/', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(5000);
-  
 }
 
 bootstrap();
